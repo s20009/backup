1f1987e6cbed9edb40948fe916effbac6aaed794
Koja Hiromu <s20009@std.it-college.ac.jp>
2020-05-23 14:29:22 +0900

    initial

---
+++ s20009/.vscode-server/bin/a9f8623ec050e5f0b44cc8ce8204a1455884749f/out/vs/platform/files/node/watcher/nsfw/watcherApp.nls.js
@@ -0,0 +1,5 @@
+/*!--------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+define("vs/platform/files/node/watcher/nsfw/watcherApp.nls",{"vs/base/node/processes":["Can't execute a shell command on a UNC drive."],"vs/platform/files/common/files":["Unknown Error"]});
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/a9f8623ec050e5f0b44cc8ce8204a1455884749f/core/vs/platform/files/node/watcher/nsfw/watcherApp.nls.js.map
